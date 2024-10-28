7d4d87ee7326d4d6fbf2cbfaa00a965f20949cd1
yungKnight <badoknight1@gmail.com>
2024-10-28 18:31:42 +0100

    This update includes the GSAP library for handling animations eiciently.

--- main.js
+++ main.js
@@ -1,33 +1,60 @@
+gsap.registerPlugin(TextPlugin);
+
 document.addEventListener("DOMContentLoaded", function() {
+
   const specialistElement = document.querySelector(".Specialist");
   const contents = ["Fullstack Web Developer", "Web Scraper", "Economist (BSc.)"];
   let currentIndex = 0;
 
-  setInterval(() => {
-    specialistElement.style.opacity = 0;
+  function typewriterEffect(element, finalText, duration) {
+    const targetEl = document.querySelector(element);
+    targetEl.textContent = "";
 
-    setTimeout(() => {
-      currentIndex = (currentIndex + 1) % contents.length;
-      specialistElement.textContent = contents[currentIndex];
+    const typingSpeed = Math.floor(duration * 1000 / finalText.length); 
+    let index = 0;
 
-      specialistElement.style.opacity = 1;
-    }, 1000); 
-  }, 3000);
-});
+    const typingInterval = setInterval(() => {
+      if (index < finalText.length) {
+        targetEl.textContent += finalText.charAt(index);
+        index++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, typingSpeed);
+  }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const listItems = document.querySelectorAll('.custom-list li');
+  typewriterEffect(".alias", " 'Kennery' ", 2);
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('in-view');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.1 });
-
-    listItems.forEach((item) => {
-        observer.observe(item);
+  function morphText() {
+    gsap.to(specialistElement, {
+      duration: 1,
+      text: contents[currentIndex],
+      ease: "power2.inOut",
+      onComplete: () => {
+        currentIndex = (currentIndex + 1) % contents.length;
+        gsap.delayedCall(4, morphText);
+      }
     });
+  }
+  morphText();
+
+  gsap.from(".languages", {
+    x: 200,
+    opacity: 0,
+    duration: 0.7,
+    ease: "power3.out",
+    stagger: 0.2
+  });
+
+  const listItems = document.querySelectorAll('.custom-list li');
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.1 });
+
+  listItems.forEach((item) => observer.observe(item));
 });
